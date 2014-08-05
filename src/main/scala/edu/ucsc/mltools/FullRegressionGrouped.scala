81599703e0d5c8029756f5012aa867277e7ba855
Kyle Ellrott <kellrott@gmail.com>
2014-08-05 16:43:50 -0700

    Testing the model after creating it

--- src/main/scala/edu/ucsc/mltools/FullRegressionGrouped.scala
+++ src/main/scala/edu/ucsc/mltools/FullRegressionGrouped.scala
@@ -1,33 +1,25 @@
 package edu.ucsc.mltools
 
 import org.rogach.scallop
-import org.apache.spark.{Partitioner, HashPartitioner, SparkContext, SparkConf}
 import org.apache.log4j.{Level, Logger}
-import java.util.concurrent.{Callable, Executors}
-import org.apache.spark.mllib.util.MLUtils
+
 import java.io.File
 import scala.io.Source.fromFile
-import org.apache.spark.mllib.classification.SVMWithSGD
-import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import scala.util.parsing.json.JSONObject
 import scala.collection.JavaConverters._
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext._
+import org.apache.spark.{Partitioner, HashPartitioner, SparkContext, SparkConf}
+
 import org.apache.spark.mllib.grouped.GroupedSVMWithSGD
 import org.apache.spark.mllib.grouped.GroupedLogisticRegressionWithSGD
-
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.rdd.RDD
-import breeze.linalg.DenseVector
-import org.apache.spark.mllib.linalg.{Vectors, Vector}
-import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.optimization.{L1Updater}
 
-import breeze.linalg.{DenseVector => BDV, SparseVector => BSV}
-import org.apache.spark.mllib.optimization.HingeGradient
-
-
-/**
- * Created by kellrott on 6/29/14.
- */
-
+/*
 class NamePartitioner(groupMap:Map[String,Int]) extends Partitioner {
   override def numPartitions: Int = groupMap.size
   override def getPartition(key: Any): Int = {
@@ -35,6 +27,8 @@ class NamePartitioner(groupMap:Map[String,Int]) extends Partitioner {
   }
   override def equals(other: Any) : Boolean = other.isInstanceOf[NamePartitioner]
 }
+*/
+
 
 object FullRegressionGrouped {
 
@@ -57,7 +51,7 @@ object FullRegressionGrouped {
     var conf = new SparkConf()
       .setMaster(cmdline.master())
       .setAppName("MLTestGrouped")
-      .set("spark.executor.memory", "10g")
+//      .set("spark.executor.memory", "14g")
       .set("spark.local.dir", cmdline.workdir())
       .set("spark.akka.frameSize", "50")
       .set("spark.akka.threads", "10")
@@ -72,8 +66,11 @@ object FullRegressionGrouped {
 
 
     val sc = new SparkContext(conf)
-    val obs_data = DataFrame.load_csv(sc, cmdline.obsFile(), separator = '\t')(x => math.log(x + 1) + 0.01)
+    val obs_data = DataFrame.load_csv(sc, cmdline.obsFile(), separator = '\t')
     val pred_data = DataFrame.load_csv(sc, cmdline.featureFile(), separator = '\t')
+
+    obs_data.rdd.cache()
+    pred_data.rdd.cache()
 
     //PropertyConfigurator.configure("log4j.config")
     Logger.getLogger("org.apache").setLevel(Level.WARN)
@@ -88,83 +85,71 @@ object FullRegressionGrouped {
 
     name_array.sliding(cmdline.groupSize(), cmdline.groupSize()).foreach( name_set => {
       println("Training %s".format( name_set.mkString(",") ))
-      val folds = name_set.filter(x => pred_data.index.contains(x)).map(x => (x, pred_data.labelJoin(obs_data, x)))
-        .map(x => (x._1, MLUtils.kFold(x._2.rdd.map(_._2), 10, seed = 11)))
+      val training_data = name_set.filter(x => pred_data.index.contains(x)).map(x => (x, pred_data.labelJoin(obs_data, x)))
+      val folds = training_data.map(x => (x._1, MLUtils.kFold(x._2.rdd.map(_._2), 10, seed = 11)))
 
       val groupSize = cmdline.groupSize()
 
       val training: RDD[(String, LabeledPoint)] = sc.union(
         folds.flatMap(x => x._2.zipWithIndex.map(y => {
-          val name = "%s:%d".format(x._1, y._2)
+          val name = "%s_%d".format(x._1, y._2)
           y._1._1.map(z => (name, z))
         }))
-      ).coalesce(cmdline.taskCount())
+      ).coalesce(cmdline.taskCount()).partitionBy( new HashPartitioner(groupSize * 10 * 2))
+
+      /*
+      println("Fold Count: %d".format(folds.size))
+      println("Training keys: %s".format( (training.keys.distinct().collect()).mkString(",")))
+      println("Training %s samples".format(training.count()))
+      */
 
       //val models = GroupedSVMWithSGD.train[String](training, cmdline.traincycles())
       val trainer = new GroupedLogisticRegressionWithSGD[String]().setIntercept(true)
       trainer.optimizer.setNumIterations(cmdline.traincycles())
+      trainer.optimizer.setUpdater(new L1Updater)
+      trainer.optimizer.setRegParam(0.1)
       val models = trainer.run(training)
+      println("Models Complete: %d".format(models.size))
+
+      //test the model
+      val fold_map = folds.toMap
+
       models.foreach(x => {
+        println("Testing: %s".format(x._1))
+        val gene_name = x._1.split("_")(0)
+        val fold_num = x._1.split(("_"))(1).toInt
+        val model = sc.broadcast(x._2)
+        x._2.clearThreshold()
+        val testing = fold_map(gene_name)(fold_num)._2
+        // Evaluate model on training examples and compute training error
+        val scoreAndLabel = testing.map { y =>
+          val prediction = model.value.predict(y.features)
+          (prediction, y.label)
+        }
+        model.unpersist()
+        val metrics = new BinaryClassificationMetrics(scoreAndLabel)
+        val auROC = metrics.areaUnderROC()
+        val auPR = metrics.areaUnderPR()
+        val stats = new JSONObject(Map( ("auROC", auROC), ("auPR", auPR)))
+
         val w = new JSONObject(Map(obs_data.index.zip(x._2.weights.toArray): _*))
-        val gene_name = x._1.split(":")(0)
-        val obj = Map(("gene", gene_name), ("weights", w), ("intercept", x._2.intercept))
+
+        val obj = Map(
+          ("gene", gene_name),
+          ("foldNumber", fold_num),
+          ("weights", w),
+          ("method", "logistic"),
+          ("intercept", x._2.intercept),
+          ("stats", stats)
+        )
         val outfile = new File(cmdline.outdir(), x._1 + ".weights.vec")
+        println("Outputting %s".format(outfile))
         val f = new java.io.FileWriter(outfile)
         f.write(new JSONObject(obj).toString())
         f.close()
-        //println(x._1, x._2)
       })
     })
     sc.stop()
   }
 
-
-  def generateCrossFold(pred_data: DataFrame, obs_data: DataFrame, gene_name: String, outdir: String): Array[Callable[Unit]] = {
-    {
-      if (pred_data.index.contains(gene_name)) {
-        val gene = pred_data.labelJoin(obs_data, gene_name)
-        //sc.setLocalProperty("spark.scheduler.pool", "pool_" + (gene_info._2 % 10))
-        val folds = MLUtils.kFold(gene.rdd.map(_._2).coalesce(2), 10, seed = 11).toIndexedSeq //.toParArray
-        //folds.tasksupport = task_support
-        //val splits = gene.rdd.map(_._2).randomSplit(Array(0.6, 0.4), seed = 11L)
-        val results = folds.zipWithIndex.map(x => {
-            val outfile = new File(outdir, gene_name + "." + x._2 + ".weights.vec")
-            new Callable[Unit] {
-              override def call(): Unit = {
-                val training = x._1._1
-                val testing = x._1._2
-                val model = SVMWithSGD.train(training, 200)
-                model.clearThreshold()
-                // Evaluate model on training examples and compute training error
-                val scoreAndLabel = testing.map { x =>
-                  val prediction = model.predict(x.features)
-                  (prediction, x.label)
-                }
-                val metrics = new BinaryClassificationMetrics(scoreAndLabel)
-                val auROC = metrics.areaUnderROC()
-
-                println("Max Balanced Accuracy", metrics.pr().map(x => (x._1 + x._2) / 2.0).max())
-                println("Area under ROC = " + auROC)
-                println("Area under PR = " + metrics.areaUnderPR)
-
-                println(metrics.precisionByThreshold().map(_._2).max)
-                //val t = metrics.precisionByThreshold().filter(_._2 > 0.1).join(metrics.recallByThreshold()).collect()
-                //println(t.length, t.mkString(","))
-                //val MSE = scoreAndLabel.map{case(v, p) => math.pow((v - p), 2)}.mean()
-                //println("training Mean Squared Error = " + MSE)
-
-                val w =  new JSONObject(Map(obs_data.index.zip(model.weights.toArray): _*))
-                val obj = Map(("gene", gene_name), ("auROC", auROC), ("weights", w), ("intercept", model.intercept)) //, ("metrics", metrics.pr().collect.mkString(" ")))
-                val f = new java.io.FileWriter(outfile)
-                f.write(new JSONObject(obj).toString())
-                f.close()
-              }
-            }
-          })
-        return results.toArray
-      }
-      return Array[Callable[Unit]]()
-    }
-
-  }
 }
