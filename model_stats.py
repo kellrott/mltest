df92df05890e6d4c6a6e71528c1593dee34e0045
Kyle Ellrott <kellrott@gmail.com>
2014-08-20 11:58:00 -0700

    Working on model analysis and application

--- model_stats.py
+++ model_stats.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
-import sys
 import os
+import sys
 import json
+import argparse
 from glob import glob
+from sklearn.metrics import auc
+import numpy
+import pandas
+import multiprocessing
 
 def recall(stats):
     return float(stats['tp']) / float(stats['tp'] + stats['fn'])
@@ -14,14 +19,150 @@ def precision(stats):
 def true_count(stats):
     return stats['fn'] + stats['tp']
 
-for a in glob(os.path.join(sys.argv[1], "*.model")):
-    with open(a) as handle:
-        txt = handle.read()
-        data = json.loads(txt)
-        thresholds = dict(list( (float(i),data['stats'][i])  for i in data['stats']))
-        mthresh = min(thresholds.keys())
-        #if mthresh > 0.01:
-        if true_count(thresholds[mthresh]) > 0:
-            #print data['gene'], precision(thresholds[mthresh]), recall(thresholds[mthresh]), true_count(thresholds[mthresh]) #, min(thresholds), max(thresholds)
-            #print data['gene'], list( (recall(thresholds[i]), i) for i in thresholds), true_count(thresholds[mthresh]) #, min(thresholds), max(thresholds)
-            print data['gene'], list( (i, thresholds[i]) for i in thresholds), true_count(thresholds[mthresh]) #, min(thresholds), max(thresholds)
+
+def run_scan(args):
+    for a in glob(os.path.join(args.models, "*.model")):
+        with open(a) as handle:
+            txt = handle.read()
+            data = json.loads(txt)
+            thresholds = dict(list( (float(i),data['stats'][i])  for i in data['stats']))
+            mthresh = min(thresholds.keys())
+            #if mthresh > 0.01:
+            if true_count(thresholds[mthresh]) > 0:
+                x = list( (i, precision(thresholds[i]), recall(thresholds[i])) for i in sorted(thresholds.keys(), reverse=True))
+                print data['gene'], auc( [0.0] + list(i[2] for i in x),  [1.0] + list(i[1] for i in x), True ), true_count(thresholds.values()[0]), list( (i, precision(thresholds[i]), recall(thresholds[i])) for i in thresholds)
+                #print data['gene'], precision(thresholds[mthresh]), recall(thresholds[mthresh]), true_count(thresholds[mthresh]) #, min(thresholds), max(thresholds)
+                #print data['gene'], list( (recall(thresholds[i]), i) for i in thresholds), true_count(thresholds[mthresh]) #, min(thresholds), max(thresholds)
+                #print data['gene'], list( (i, precision(thresholds[i]), recall(thresholds[i])) for i in thresholds), true_count(thresholds[mthresh]) #, min(thresholds), max(thresholds)
+
+def run_prscore(args):
+    
+    gene_scores = {}
+    
+    for a in glob(os.path.join(args.models, "*.model")):
+        with open(a) as handle:
+            txt = handle.read()
+            data = json.loads(txt)
+            thresholds = dict(list( (float(i),data['stats'][i])  for i in data['stats']))
+            mthresh = min(thresholds.keys())
+            gene = data['gene']
+            if gene not in gene_scores:
+                gene_scores[gene] = []
+            if true_count(thresholds[mthresh]) > 0:
+                x = list( (i, precision(thresholds[i]), recall(thresholds[i])) for i in sorted(thresholds.keys(), reverse=True))
+                pr_vals = list(i[2] for i in x)
+                rc_vals = list(i[1] for i in x)
+                a = auc( [0.0] + rc_vals, [1.0] + pr_vals, True )
+                #a = auc( [1.0] + pr_vals, [0.0] + rc_vals, True )
+                gene_scores[gene].append(a)
+
+    matrix = None
+    if args.original is not None:
+        matrix = pandas.read_csv(args.original, sep="\t", index_col=0)
+
+    for gene in gene_scores:
+        vals = gene_scores[gene] + [0.0] * (args.folds-len(gene_scores[gene]))
+        if matrix is None or gene not in matrix.columns :
+            print gene, numpy.mean(vals), numpy.std(vals)
+        else:
+            base = float(sum(matrix[gene] == 1)) / float(len(matrix[gene]))
+            print gene, numpy.mean(vals), numpy.std(vals), base, (numpy.mean(vals) - base) / numpy.std(vals)
+
+
+def run_ratios(args):
+    matrix = pandas.read_csv(args.labels, sep="\t", index_col=0)
+    for i in matrix.columns:
+        if args.mode == "ratio":
+            print i, float(sum(matrix[i] == 1)) / float(len(matrix[i]))
+        if args.mode == "count":
+            print i, sum(matrix[i] == 1)
+
+
+
+class Model:
+
+    def __init__(self, path):
+        with open(path) as handle:
+            data = handle.read()
+            self.model_data = json.loads(data)
+        self.weights =  pandas.Series(self.model_data['weights'])
+        self.intercept = self.model_data['intercept']
+        self.gene = self.model_data['gene']
+
+    def predict(self, sample):
+        common = self.weights.index.union(sample.index)
+        if len(common) == 0:
+            raise Exception("No model overlap")
+        left = self.weights.reindex(index=common, copy=False, fill_value=0.0)
+        right = sample.reindex(index=common, copy=False, fill_value=0.0)
+
+        margin = left.dot(right) + self.intercept
+        try:
+            score = 1.0/ (1.0 + numpy.exp(-margin))
+            return score
+        except OverflowError:
+            return numpy.nan
+
+
+def model_predict(args):
+    model_path, matrix = args
+    print "Predicting", model_path
+    model = Model(model_path)
+    predict = {}
+    for c in matrix.columns:
+        sample = matrix[c]
+        p = model.predict(sample)
+        predict[c] = p
+    return model.gene, predict
+
+
+def run_model(args):
+
+    matrix = pandas.read_csv(args.matrix, sep="\t", index_col=0).replace(numpy.nan, 0.0)
+
+    gene_set = {}
+    for model_path in glob(os.path.join(args.models, "*")):
+        with open(model_path) as handle:
+            txt = handle.read()
+            data = json.loads(txt)
+            gene_set[data['gene']] = model_path
+
+    out = pandas.DataFrame(columns=matrix.columns, index=gene_set.keys())
+
+    pool = multiprocessing.Pool(8)
+    for value in pool.map(model_predict, ( (i, matrix) for i in gene_set.values() )):
+        print value
+        
+    
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title="subcommand")
+
+    parser_scan = subparsers.add_parser('scan')
+    parser_scan.add_argument("models")
+    parser_scan.set_defaults(func=run_scan)
+
+    parser_prscore = subparsers.add_parser('prscore')
+    parser_prscore.add_argument("models")
+    parser_prscore.add_argument("--folds", default=1)
+    parser_prscore.add_argument("--original", default=None)
+    parser_prscore.set_defaults(func=run_prscore)
+
+    parser_ratios = subparsers.add_parser('labels')
+    parser_ratios.add_argument("labels")
+    parser_ratios.add_argument("mode")
+    parser_ratios.set_defaults(func=run_ratios)  
+    
+    parser_model = subparsers.add_parser('model')
+    parser_model.add_argument("models")
+    parser_model.add_argument("matrix")
+    parser_model.set_defaults(func=run_model)  
+    
+    
+    args = parser.parse_args()
+    sys.exit(args.func(args))
+
+
--- src/main/scala/edu/ucsc/mltools/PredictMatrix.scala
+++ src/main/scala/edu/ucsc/mltools/PredictMatrix.scala
@@ -5,6 +5,12 @@ import org.apache.spark.{SparkContext, SparkConf}
 import java.io.File
 import scala.util.parsing.json.JSON
 import scala.io.Source
+import scala.io.Source._
+import scala.Some
+import com.fasterxml.jackson.databind.ObjectMapper
+import scala.collection.JavaConverters._
+import org.apache.spark.mllib.classification.LogisticRegressionModel
+import org.apache.spark.mllib.linalg.{Vectors => MLVectors}
 
 /**
  * Created by kellrott on 6/29/14.
@@ -17,8 +23,8 @@ object PredictMatrix {
       val cores : scallop.ScallopOption[String] = opt[String]("cores", default = Some("32"))
       val workdir : scallop.ScallopOption[String] = opt[String]("workdir", default = Some("/tmp"))
 
-      val vectorDir: scallop.ScallopOption[String] = trailArg[String](required = true)
-      val matrixFile: scallop.ScallopOption[String] = trailArg[String](required = true)
+      val modeldir: scallop.ScallopOption[String] = trailArg[String](required = true)
+      val obsfile: scallop.ScallopOption[String] = trailArg[String](required = true)
       val outFile: scallop.ScallopOption[String] = trailArg[String](required = true)
     }
 
@@ -33,19 +39,41 @@ object PredictMatrix {
 
     val sc = new SparkContext(conf)
 
-    val files = new File(cmdline.vectorDir()).listFiles().filter( f => """.vec""".r.findFirstIn(f.getName).isDefined)
-    val files_rdd = sc.parallelize(files.slice(0,5))
-    val data = files_rdd.map( x => JSON.parseFull(Source.fromFile(x).getLines().mkString("")).get.asInstanceOf[Map[String,Any]] )
-    val mf = LinearModelFrame.create(data.map(x => {
-      val label = x("gene").asInstanceOf[String]
-      val intercept = x("intercept").asInstanceOf[Double]
-      val weights = x("weights").asInstanceOf[Map[String,Double]]
-      (label, intercept, weights)
-    }))
+    val fileset = sc.parallelize(new File(cmdline.modeldir()).listFiles().map( _.getAbsolutePath ), 50)
+    val model_objects = fileset.map( x => {
+      try {
+        val jsontxt = fromFile(x).mkString
+        new ObjectMapper().readValue(jsontxt, classOf[java.util.Map[String, AnyRef]])
+      } catch {
+        case e: Exception => (Map[String,AnyRef]().asJava)
+      }
+    } ).filter( _.containsKey("gene"))
 
-    val features = DataFrame.load_csv(sc, cmdline.matrixFile(), '\t')(x => math.log(x+1) + 0.01)
+    val obs_data = DataFrame.load_csv(sc, cmdline.obsfile(), separator = '\t')
+    val index_br = obs_data.index_br
 
-    val out = mf.predict(features)
+    val models = model_objects.map( x => {
+      val weight_map = x.get("weights").asInstanceOf[java.util.Map[String,Double]].asScala
+      if (weight_map.keys.filter(index_br.value.contains(_)).size == 0) {
+        throw new RuntimeException("No symbol overlap in model")
+      }
+      val weights = index_br.value.map( y => weight_map.getOrElse(y, 0.0) ).toArray
+      val model = new LogisticRegressionModel(MLVectors.dense(weights), x.get("intercept").asInstanceOf[Double])
+      model.clearThreshold()
+      if (x.containsKey("foldNumber")) {
+        ("%s_%s".format(x.get("gene").asInstanceOf[String], x.get("foldNumber")), model)
+      } else {
+        (x.get("gene").asInstanceOf[String], model)
+      }
+    } ).filter(_._1 != null).collect()
+
+    val models_br = sc.broadcast(models)
+
+    val predictions = obs_data.rdd.map( x => {
+      (x._1, models_br.value.map( y => (y._1, y._2.predict(x._2) ) ).toMap)
+    })
+
+    val out = DataFrame.create(predictions)
     out.write_csv(cmdline.outFile(), '\t')
 
   }
